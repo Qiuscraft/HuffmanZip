4a911bab31e39c491f3ceed826655125180b06ce
qiuscraft <2811883713@qq.com>
2024-12-03 16:34:48 +0800

    优化HuffmanZip代码风格

--- HuffmanZip.cpp
+++ HuffmanZip.cpp
@@ -19,49 +19,64 @@ void HuffmanZip::compress(const std::string& inputPath, const std::string& outpu
     if (!outFile.is_open()) 
         throw std::runtime_error("Failed to open output file");
 
-    // Build Huffman tree
-    uint64_t countArray[256];
-
     std::filesystem::path path(inputPath);
 
     if (!std::filesystem::exists(path)) {
         throw std::runtime_error("Failed to open input file");
-    } 
-    
-    else if (std::filesystem::is_regular_file(path)) {
-        uint32_t emptyDirSize = 0;
-        outFile.write((char *) &emptyDirSize, sizeof(emptyDirSize));
-        Counter::writeCountArray(inputPath, countArray);
-        HuffmanTree tree(countArray);
-        writeTree(outFile, tree, password);
-        addFileToZip(inputPath, outFile, tree);
-    } 
-    
-    else if (std::filesystem::is_directory(path)) {
-        std::string emptyDir;
-        for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
-            if (is_directory_empty(entry.path())) {
-                emptyDir = emptyDir.append(entry.path().string()).append("\n");
-            }
-        }
-        uint32_t emptyDirSize = emptyDir.size();
-        outFile.write((char *) &emptyDirSize, sizeof(emptyDirSize));
-        outFile.write(emptyDir.c_str(), emptyDirSize);
-
-        Counter::writeDirectoryCountArray(inputPath, countArray);
-        HuffmanTree tree(countArray);
-        writeTree(outFile, tree, password);
-        for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
-            if (std::filesystem::is_regular_file(entry.path())) {
-                addFileToZip(entry.path().string(), outFile, tree);
-            }
-        }
     }
-    
+
+    if (std::filesystem::is_regular_file(path)) {
+        handleFileCompression(inputPath, outFile, password);
+    } 
+    else if (std::filesystem::is_directory(path)) {
+        handleDirectoryCompression(inputPath, outFile, password);
+    } 
     else {
         throw std::runtime_error("The inputPath is neither a regular file nor a directory.");
     }
     outFile.close();
+}
+
+void HuffmanZip::handleFileCompression(const std::string& inputPath, std::ofstream &outFile, const std::string &password) {
+    // 向压缩包写入空文件夹数据
+    uint32_t emptyDirSize = 0;
+    outFile.write((char *) &emptyDirSize, sizeof(emptyDirSize));
+    // 向压缩包写入树数据
+    uint64_t countArray[256];
+    Counter::writeCountArray(inputPath, countArray);
+    HuffmanTree tree(countArray);
+    writeTree(outFile, tree, password);
+    // 向压缩包写入每个文件的数据
+    addFileToZip(inputPath, outFile, tree);
+}
+
+void HuffmanZip::handleDirectoryCompression(const std::string& inputPath, std::ofstream &outFile, const std::string &password) {
+    // 向压缩包写入空文件夹数据
+    writeEmptyDirectories(outFile, inputPath);
+    // 向压缩包写入树数据
+    uint64_t countArray[256];
+    Counter::writeDirectoryCountArray(inputPath, countArray);
+    HuffmanTree tree(countArray);
+    writeTree(outFile, tree, password);
+    // 向压缩包写入每个文件的数据
+    for (const auto& entry : std::filesystem::recursive_directory_iterator(inputPath)) {
+        if (std::filesystem::is_regular_file(entry.path())) {
+            addFileToZip(entry.path().string(), outFile, tree);
+        }
+    }
+}
+
+// 向压缩包中写入空文件夹数据
+void HuffmanZip::writeEmptyDirectories(std::ofstream &outFile, const std::string& inputPath) {
+    std::string emptyDir;
+    for (const auto& entry : std::filesystem::recursive_directory_iterator(inputPath)) {
+        if (is_directory_empty(entry.path())) {
+            emptyDir = emptyDir.append(entry.path().string()).append("\n");
+        }
+    }
+    uint32_t emptyDirSize = emptyDir.size();
+    outFile.write((char *) &emptyDirSize, sizeof(emptyDirSize));
+    outFile.write(emptyDir.c_str(), emptyDirSize);
 }
 
 void HuffmanZip::decompress(const std::string& inputPath, const std::string& outputPath) {
@@ -73,111 +88,117 @@ void HuffmanZip::decompress(const std::string& inputPath, const std::string& out
     if (!inFile.is_open()) 
         throw std::runtime_error("Failed to open input file");
 
-    // Read empty dir
+    // 读取空文件夹数据并创建空文件夹
     uint32_t dirSize;
     inFile.read((char*)&dirSize, sizeof(dirSize));
     std::string dir(dirSize, '\0');
     inFile.read(&dir[0], dirSize);
     createDirectories(dir, outputPath);
 
+    // 读取树
     HuffmanTree tree = readTree(inFile, password);
 
+    // 解压每一个文件。
     while (true) {
-        // Read OutFile
-        uint32_t filenameSize;
-        inFile.read((char*)&filenameSize, sizeof(filenameSize));
-
-        if (inFile.eof())
+        if (!decompressFile(inFile, outputPath, tree)) {
             break;
-        
-        std::string filename(filenameSize, '\0');
-        inFile.read(&filename[0], filenameSize);
-
-        std::filesystem::path outputFullPath = std::filesystem::path(outputPath) / filename;
-
-        // Ensure that the directory exists
-        std::filesystem::create_directories(outputFullPath.parent_path());
-
-        std::ofstream outFile(outputFullPath, std::ios::binary);
-        if (!outFile.is_open()) 
-            throw std::runtime_error("Failed to open output file");
-
-        // Read bitAmount and paddingBitAmount
-        uint64_t bitAmount;
-        uint32_t paddingBitAmount;
-        inFile.read((char*)&bitAmount, sizeof(bitAmount));
-        inFile.read((char*)&paddingBitAmount, sizeof(paddingBitAmount));
-
-        uint64_t actualBits = bitAmount - paddingBitAmount;
-
-        // Read compressed data and decode
-        BitInBuffer buffer(inFile);
-        HuffmanNode* currentNode = tree.getRoot();
-
-        for (uint64_t i = 0; i < actualBits; ++i) {
-            bool bit = buffer.readBit();
-            
-            if (bit) {
-                currentNode = currentNode->rightChild;
-            } else {
-                currentNode = currentNode->leftChild;
-            }
-
-            if (currentNode->isLeaf()) {
-                outFile.put(currentNode->getCharacter());
-                currentNode = tree.getRoot();
-            }
         }
-
-        outFile.close();
     }
 
     inFile.close();
 }
 
-void HuffmanZip::addFileToZip(const std::string& inputPath, std::ofstream& outFile, const HuffmanTree &tree) {
-    std::ifstream inFile(inputPath, std::ios::binary);
-    if (!inFile.is_open()) 
-        throw std::runtime_error("Failed to open input file");
+// 解压文件，解压成功返回true，当走到压缩包末尾，没有文件可以解压时返回false。
+bool HuffmanZip::decompressFile(std::ifstream &inFile, const std::string& outputPath, const HuffmanTree &tree) {
+    uint32_t filenameSize;
+    inFile.read((char*)&filenameSize, sizeof(filenameSize));
 
+    if (inFile.eof())
+        return false;
+    
+    // 读取文件名
+    std::string filename(filenameSize, '\0');
+    inFile.read(&filename[0], filenameSize);
+
+    // 读取并创建文件所在的文件夹
+    std::filesystem::path outputFullPath = std::filesystem::path(outputPath) / filename;
+    std::filesystem::create_directories(outputFullPath.parent_path());
+
+    std::ofstream outFile(outputFullPath, std::ios::binary);
+    if (!outFile.is_open()) 
+        throw std::runtime_error("Failed to open output file");
+
+    // 读取文件实际长度
+    uint64_t bitAmount;
+    uint32_t paddingBitAmount;
+    inFile.read((char*)&bitAmount, sizeof(bitAmount));
+    inFile.read((char*)&paddingBitAmount, sizeof(paddingBitAmount));
+
+    uint64_t actualBits = bitAmount - paddingBitAmount;
+
+    decodeContent(inFile, outFile, tree, actualBits);
+    outFile.close();
+    return true;
+}
+
+void HuffmanZip::decodeContent(std::ifstream &inFile, std::ofstream &outFile, const HuffmanTree &tree, uint64_t actualBits) {
+    BitInBuffer buffer(inFile);
+    HuffmanNode* currentNode = tree.getRoot();
+
+    for (uint64_t i = 0; i < actualBits; ++i) {
+        bool bit = buffer.readBit();
+        currentNode = bit ? currentNode->rightChild : currentNode->leftChild;
+
+        if (currentNode->isLeaf()) {
+            outFile.put(currentNode->getCharacter());
+            currentNode = tree.getRoot();
+        }
+    }
+}
+
+void HuffmanZip::addFileToZip(const std::string& inputPath, std::ofstream& outFile, const HuffmanTree &tree) {
     uint32_t filenameSize = inputPath.size();
     outFile.write((char *) &filenameSize, sizeof(filenameSize));
     outFile.write(inputPath.c_str(), filenameSize);
 
-    // Reserve space for bitAmount and paddingBitAmount
+    // 预留位置以便回写 bitAmount 和 paddingBitAmount
     uint64_t bitAmount = 0;
     uint32_t paddingBitAmount = 0;
-    
-    // Record the current position to write bitAmount and paddingBitAmount later
     std::streampos bitAmountPos = outFile.tellp();
     outFile.write((char *) &bitAmount, sizeof(bitAmount)); // bitAmount
     outFile.write((char *) &paddingBitAmount, sizeof(paddingBitAmount)); // paddingBitAmount
 
-    // Read input file and write Huffman codes to output file
-    std::unordered_map<char, HuffmanCode> codeMap = tree.getCodeMap();
-    char c;
+    // 压缩数据并计算比特数量
+    writeCompressedData(outFile, tree.getCodeMap(), bitAmount, paddingBitAmount, inputPath);
 
+    // 移动到预留位置并写入实际值
+    outFile.seekp(bitAmountPos);
+    if (!outFile) 
+        throw std::runtime_error("Failed to seek to the reserved position in file");
+    outFile.write((char *) &bitAmount, sizeof(bitAmount));
+    outFile.write((char *) &paddingBitAmount, sizeof(paddingBitAmount));
+
+    // 移动到文件末尾
+    outFile.seekp(0, std::ios::end); 
+    if (!outFile) 
+        throw std::runtime_error("Failed to seek to the end of file");
+}
+
+// 压缩数据并计算比特数量
+void HuffmanZip::writeCompressedData(std::ofstream &outFile, const std::unordered_map<char, HuffmanCode>& codeMap, uint64_t &bitAmount, uint32_t &paddingBitAmount, const std::string &inputPath) {
+    std::ifstream inFile(inputPath, std::ios::binary);
+    if (!inFile.is_open()) 
+        throw std::runtime_error("Failed to open input file");
+
+    char c;
     BitOutBuffer buffer(outFile);
     while (inFile.get(c)) {
-        buffer.write(codeMap[c]);
-        bitAmount += codeMap[c].getSize();
+        buffer.write(codeMap.at(c));
+        bitAmount += codeMap.at(c).getSize();
     }
 
     paddingBitAmount = buffer.finalWriteWithPaddings();
     bitAmount += paddingBitAmount;
-
-    // Move to the recorded position and write the actual values
-    outFile.seekp(bitAmountPos);
-    if (!outFile) 
-        throw std::runtime_error("Failed to seek to the reserved position in file");
-
-    outFile.write((char *) &bitAmount, sizeof(bitAmount));
-    outFile.write((char *) &paddingBitAmount, sizeof(paddingBitAmount));
-
-    // Move to the end of the file
-    outFile.seekp(0, std::ios::end); 
-    if (!outFile) 
-        throw std::runtime_error("Failed to seek to the end of file");
 
     inFile.close();
 }
@@ -190,7 +211,6 @@ void HuffmanZip::writeTree(std::ofstream& outFile, const HuffmanTree &tree, cons
     std::string serializedTree = XOREncryption::encryptData(tree.serialize(), password);
     uint32_t treeSize = serializedTree.size();
 
-    // Write tree size and serialized tree to output file
     outFile.write((char *) &treeSize, sizeof(treeSize));
     outFile.write(serializedTree.c_str(), treeSize);
 }
@@ -200,7 +220,6 @@ HuffmanTree HuffmanZip::readTree(std::ifstream &inFile) {
 }
 
 HuffmanTree HuffmanZip::readTree(std::ifstream &inFile, const std::string &password) {
-    // Read tree
     uint32_t treeSize;
     inFile.read((char*)&treeSize, sizeof(treeSize));
     std::string serializedTree(treeSize, '\0');
--- HuffmanZip.h
+++ HuffmanZip.h
@@ -16,7 +16,16 @@ public:
     static void decompress(const std::string& inputPath, const std::string& outputPath, const std::string &password);
     static bool is_directory_empty(const std::filesystem::path& dirPath);
 private:
+    static void handleFileCompression(const std::string& inputPath, std::ofstream &outFile, const std::string &password);
+    static void handleDirectoryCompression(const std::string& inputPath, std::ofstream &outFile, const std::string &password);
+    static void writeEmptyDirectories(std::ofstream &outFile, const std::string& inputPath);
+
+    static bool decompressFile(std::ifstream &inFile, const std::string& outputPath, const HuffmanTree &tree);
+    static void decodeContent(std::ifstream &inFile, std::ofstream &outFile, const HuffmanTree &tree, uint64_t actualBits);
+
     static void addFileToZip(const std::string& inputPath, std::ofstream& outFile, const HuffmanTree &tree);
+    static void writeCompressedData(std::ofstream &outFile, const std::unordered_map<char, HuffmanCode>& codeMap, uint64_t &bitAmount, uint32_t &paddingBitAmount, const std::string &inputPath);
+    
     static void writeTree(std::ofstream& outFile, const HuffmanTree &tree);
     static void writeTree(std::ofstream& outFile, const HuffmanTree &tree, const std::string &password);
     static HuffmanTree readTree(std::ifstream &inFile);
