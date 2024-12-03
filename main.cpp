6d845e7577e9ac9f173f7bd6a674460b32bc4c09
qiuscraft <2811883713@qq.com>
2024-12-03 20:20:15 +0800

    main.cpp remove std::

--- main.cpp
+++ main.cpp
@@ -5,55 +5,57 @@
 #include <string>
 #include <chrono>
 
+using namespace std;
+
 int main(int argc, char *argv[]) {
     // 计时用
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = chrono::high_resolution_clock::now();
 
-    std::string mode(argv[1]);
-    std::string input(argv[2]);
+    string mode(argv[1]);
+    string input(argv[2]);
 
-    std::string output;
+    string output;
     if (argc > 3) output = argv[3];
     else {
         if (mode == "-c" || mode == "-compress") output = input + ".huffmanzip";
         else output = ".";
     }
 
-    std::string password;
+    string password;
     bool overwrite = false;
 
     for (int i = 4; i < argc; ++i) {
-        if (std::string(argv[i]) == "-p" && i + 1 < argc) {
+        if (string(argv[i]) == "-p" && i + 1 < argc) {
             password = argv[++i];
-        } else if (std::string(argv[i]) == "-y") {
+        } else if (string(argv[i]) == "-y") {
             overwrite = true;
         }
     }
 
     if (mode == "-c" || mode == "-compress") {
         try {
-            HuffmanZip::compress(input, output, password, std::cin, std::cout, overwrite);
-        } catch (const std::exception& e) {
-            std::cerr << e.what() << std::endl;
+            HuffmanZip::compress(input, output, password, cin, cout, overwrite);
+        } catch (const exception& e) {
+            cerr << e.what() << endl;
             return 1;
         }
     } else if (mode == "-d" || mode == "-decompress") {
         try {
-            HuffmanZip::decompress(input, output, password, std::cin, std::cout, overwrite);
-        } catch (const std::exception& e) {
-            std::cerr << e.what() << std::endl;
+            HuffmanZip::decompress(input, output, password, cin, cout, overwrite);
+        } catch (const exception& e) {
+            cerr << e.what() << endl;
             return 1;
         }
     } else {
-        std::cerr << "Usage:\n";
-        std::cerr << "  huffmanzip -c(ompress) <input> [<output>] [-p <password>] [-y]\n";
-        std::cerr << "  huffmanzip -d(ecompress) <input> [<output>] [-p <password>] [-y]\n";
+        cerr << "Usage:\n";
+        cerr << "  huffmanzip -c(ompress) <input> [<output>] [-p <password>] [-y]\n";
+        cerr << "  huffmanzip -d(ecompress) <input> [<output>] [-p <password>] [-y]\n";
         return 1;
     }
 
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    std::cout << "It took " << duration.count() << " seconds." << std::endl;
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+    cout << "It took " << duration.count() << " seconds." << endl;
 
     return 0;
 }
