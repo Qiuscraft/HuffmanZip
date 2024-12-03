6662d5c38fe27008cb66dcd7205690c8476ae7ee
qiuscraft <2811883713@qq.com>
2024-12-03 21:38:22 +0800

    main.cpp 修复参数识别

--- main.cpp
+++ main.cpp
@@ -11,27 +11,59 @@ int main(int argc, char *argv[]) {
     // 计时用
     auto start = chrono::high_resolution_clock::now();
 
-    string mode(argv[1]);
-    string input(argv[2]);
-
-    string output;
-    if (argc > 3) output = argv[3];
-    else {
-        if (mode == "-c" || mode == "-compress") output = input + ".huffmanzip";
-        else output = ".";
+    if (argc < 3) {
+        cerr << "Usage:\n";
+        cerr << "  huffmanzip -c(ompress) <input> [<output>] [-p <password>] [-y]\n";
+        cerr << "  huffmanzip -d(ecompress) <input> [<output>] [-p <password>] [-y]\n";
+        return 1;
     }
 
+    string mode = argv[1];
+    string input = argv[2];
+
+    string output;
     string password;
     bool overwrite = false;
 
-    for (int i = 4; i < argc; ++i) {
-        if (string(argv[i]) == "-p" && i + 1 < argc) {
-            password = argv[++i];
-        } else if (string(argv[i]) == "-y") {
+    // Initialize to check if optional arguments have been reached
+    bool optionalArgsStarted = false;
+
+    // Start parsing from the third argument
+    for (int i = 3; i < argc; ++i) {
+        string arg = argv[i];
+
+        if (arg == "-p") {
+            if (i + 1 < argc) {
+                password = argv[++i];
+            } else {
+                cerr << "Error: Missing password after -p option.\n";
+                return 1;
+            }
+        } else if (arg == "-y") {
             overwrite = true;
+        } else if (arg.size() > 0 && arg[0] == '-') {
+            cerr << "Unknown option: " << arg << endl;
+            return 1;
+        } else if (!optionalArgsStarted) {
+            // Assume it's the output file if no optional arguments have started
+            output = arg;
+            optionalArgsStarted = true;
+        } else {
+            cerr << "Unexpected argument: " << arg << endl;
+            return 1;
         }
     }
 
+    // Set a default output filename if none was provided
+    if (output.empty()) {
+        if (mode == "-c" || mode == "-compress") {
+            output = input + ".huffmanzip";
+        } else {
+            output = ".";
+        }
+    }
+
+    // Invoke the corresponding compress or decompress function
     if (mode == "-c" || mode == "-compress") {
         try {
             HuffmanZip::compress(input, output, password, cin, cout, overwrite);
